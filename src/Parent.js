fbca2f4c052dc9d57fc7b380d228ae1f5fb25e7f
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-19 17:54:05 -0700

    initial commit

--- src/Parent.js
+++ src/Parent.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
-import { getRandomColor } from "./randomColorGenerator.js";
-import Child from "./Child";
+import React, { useState } from 'react';
+import { getRandomColor } from './randomColorGenerator.js';
+import Child from './Child';
 
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState('#FFF');
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setChildrenColor(newRandomColor);
+    setColor(newChildColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={handleChangeColor} color={childrenColor} />
+      <Child onChangeColor={handleChangeColor} color={childrenColor} />
     </div>
   );
 }
